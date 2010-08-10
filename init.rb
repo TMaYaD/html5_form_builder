474fe96ccc99986a4b752c27894b056f1cbdad7c
Subhash Chandra <TMaYaD@loonyb.in>
2010-08-10 12:33:48 +0530

    initial commit

---
+++ init.rb
@@ -0,0 +1,3 @@
+# Include hook code here
+
+require 'html5_form_builder'
---
+++ lib/generators/html5_form_builder_generator.rb
@@ -0,0 +1,3 @@
+class Html5FormBuilderGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+end
